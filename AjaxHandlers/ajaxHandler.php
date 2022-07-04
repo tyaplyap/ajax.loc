cce03a6a4e65c56da81db19a657ebc0df807a598
tyaplyap <ivan.tretiakov.ru@gmail.com>
2022-07-04 23:50:42 +0700

    Update ajaxHandler.php

--- AjaxHandlers/ajaxHandler.php
+++ AjaxHandlers/ajaxHandler.php
@@ -12,15 +12,20 @@
 		$searchRequest = $_POST['name'];
 		
 		// Оставить в тексте только буквы, цифры и пробел
+		// Все знаки препинания, кавычки и прочее будут убраны (заменены на пустую строку)
 		$searchRequest = mb_eregi_replace("[^a-zа-яё0-9 ]", '', $searchRequest);
 		
 		// Триммируем
 		$searchRequest = trim($searchRequest);
 		
+		// Задаем шаблон поиска для подстановки в sql
+		// В соответствии с шаблоном будут найдены значения, начинающиеся с $searchRequest
+		$searchRequest = $searchRequest . '%';
+		
 		// Подключаемся к базе данных, выполняем запрос
 		$dbh = new \PDO('mysql:dbname=experimental;host=localhost;charset=UTF8', 'root', '');
 		$sth = $dbh->prepare('SELECT * FROM `users` WHERE `name` LIKE :search ORDER BY `name` LIMIT 10');
-		$sth->execute([':search' => $searchRequest . '%']);
+		$sth->execute([':search' => $searchRequest]);
 		
 		// Получаем результаты из базы данных в виде ассоциативного массива
 		$searchResults = $sth->fetchAll(\PDO::FETCH_ASSOC);
